f2526575597a6d969ce84311dfc49af76ac424b1
William Can <wilenrike@gmail.com>
2021-11-05 02:21:20 -0600

    add: Basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
     
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/Redes2_Page'>
             <Switch>
                 <Route exact path='/asistencia'>
                     <Attendance/>
